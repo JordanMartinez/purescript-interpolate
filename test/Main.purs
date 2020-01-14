40735ec89d03a2b25928aec982cd0069787f7806
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-01-13 18:39:31 -0800

    Reduce interpolatable values to 5 types

--- test/Main.purs
+++ test/Main.purs
@@ -23,7 +23,9 @@ main = launchAff_ $ runSpec [consoleReporter] do
       i 'a' 'b' 'c' `shouldEqual` "'a''b''c'"
     it "Interpolating strings works" do
       i "a" "b" "c" `shouldEqual` "abc"
-    it "Interpolating arrays works" do
-      i [1] [2] [3, 4] `shouldEqual` "[1][2][3,4]"
     it "Interpolating values with different types works" do
       i 0 1.0 true 'a' "a" `shouldEqual` "01.0true'a'a"
+
+    -- Intentional: this will fail to compile
+    -- it "Interpolating values from unaccepted types will produce a compiler error" do
+    --   i [1, 2, 3] `shouldEqual` "[1, 2, 3]"
