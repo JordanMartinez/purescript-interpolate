c879ad51e5450abeeed289272bcb0456379d2169
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-01-14 18:40:49 -0800

    Add benchmark using `foldl`
    - Note: I got an OOM error when doing this on an array of 30 elements.

---
+++ benchmark/Benchmark.purs
@@ -0,0 +1,27 @@
+module Benchmark where
+
+import Prelude
+import Effect (Effect)
+import Data.Array ((..))
+import Data.Foldable (foldl)
+import Data.Interpolate (i)
+import Test.QuickCheck.Arbitrary (arbitrary)
+import Test.QuickCheck.Gen (Gen, vectorOf)
+import Benchotron.Core (Benchmark, benchFn, mkBenchmark)
+import Benchotron.UI.Console (runSuite)
+
+main :: Effect Unit
+main = runSuite [benchFoldl]
+
+benchFoldl :: Benchmark
+benchFoldl = mkBenchmark
+  { slug: "sum"
+  , title: "Comparing normal `a <> b` to `i a b`"
+  , sizes: (1..20)
+  , sizeInterpretation: "Number of elements in the array"
+  , inputsPerSize: 1
+  , gen: \n -> vectorOf n (arbitrary :: Gen Int)
+  , functions: [ benchFn "a <> b" (\array -> foldl (\a b -> show a <> show b) "" array)
+               , benchFn "i a b" (\array -> foldl i "" array)
+               ]
+  }
---
+++ benchmark/README.md
@@ -0,0 +1,5 @@
+# Benchmarks
+
+Run the benchmark in this folder via `pulp run -I 'benchmark' -m Benchmark`
+
+In short, if you're going to do a `foldl`/`foldr`, use `show` and `<>` explicitly rather than implicitly via `i`. See the `benchmark-results` folder.
