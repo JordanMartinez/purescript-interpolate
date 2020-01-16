a89faf2de1395eebdafb6e51ed36266bf57a5e20
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-01-16 06:36:13 -0800

    Update benchmark with higher number of elements in the array

--- benchmark/Benchmark.purs
+++ benchmark/Benchmark.purs
@@ -13,47 +13,17 @@ import Benchotron.UI.Console (runSuite)
 main :: Effect Unit
 main = runSuite
   [ benchFoldlBoth
-  , benchFoldlAppendOnly
-  , benchFoldlInterpOnly
   ]
 
 benchFoldlBoth :: Benchmark
 benchFoldlBoth = mkBenchmark
   { slug: "foldl-compare-append-and-i"
   , title: """`foldl acc "" arrayOfInts` where `acc` is `a <> show b` or `i`"""
-  , sizes: (1..25)
-      -- ^ anything higher than 30 elements produces OOM error
-      -- for 'show a <> show b'
+  , sizes: (1..5) <#> (_ * 1000)
   , sizeInterpretation: "Number of elements in the array"
   , inputsPerSize: 1
   , gen: \n -> vectorOf n (arbitrary :: Gen Int)
   , functions: [ benchFn "a <> b" (foldl (\acc next -> acc <> show next) "")
                , benchFn "i a b" (foldl i "")
-               ]
-  }
-
-benchFoldlAppendOnly :: Benchmark
-benchFoldlAppendOnly = mkBenchmark
-  { slug: "foldl-append-only"
-  , title: """`foldl (\a b -> show a <> show b) "" arrayOfInts`"""
-  , sizes: (1..25)
-      -- ^ anything higher than 30 elements produces OOM error
-      -- for 'show a <> show b'
-  , sizeInterpretation: "Number of elements in the array"
-  , inputsPerSize: 1
-  , gen: \n -> vectorOf n (arbitrary :: Gen Int)
-  , functions: [ benchFn "a <> b" (foldl (\acc next -> acc <> show next) "")
-               ]
-  }
-
-benchFoldlInterpOnly :: Benchmark
-benchFoldlInterpOnly = mkBenchmark
-  { slug: "foldl-interp-only"
-  , title: """`foldl i "" arrayOfInts`"""
-  , sizes: (1..5) <#> (_ * 1000)
-  , sizeInterpretation: "Number of elements in the array"
-  , inputsPerSize: 1
-  , gen: \n -> vectorOf n (arbitrary :: Gen Int)
-  , functions: [ benchFn "i a b" (foldl i "")
                ]
   }
